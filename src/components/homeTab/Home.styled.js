baa82809c0b628ec920bc5fafc1764cf0559a743
Ksenia Pilshchikova <ksushapi@gmail.com>
2022-12-16 12:26:20 +0100

    Update Home.styled.js

--- src/components/homeTab/Home.styled.js
+++ src/components/homeTab/Home.styled.js
@@ -3,14 +3,13 @@ import styled from "@emotion/styled";
 export const HomeWrap = styled.div`
 width: 100%;
  position:relative;
- 
  padding-left: 10px;
  overflow-y: scroll;
  overflow-x: initial;
 
     @media screen and (max-width: 767px) {
     width: 100%;
-    height: 338px;
+    height: 100%;
     }
     @media screen and (min-width: 768px) and (max-width: 1279px) {
     width: 750px;
